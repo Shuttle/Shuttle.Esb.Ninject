8cd15a25d8c91d1c986c16be5af3dba1965af6dc
Eben <me@ebenroux.co.za>
2016-09-04 08:58:23 +0200

    - v7.0.0

--- Shuttle.Esb.Ninject/Properties/AssemblyInfo.cs
+++ Shuttle.Esb.Ninject/Properties/AssemblyInfo.cs
@@ -25,10 +25,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Esb.Ninject for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
 [assembly: AssemblyProduct("Shuttle.Esb")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyInformationalVersion("7.0.0")]
 [assembly: ComVisible(false)]
